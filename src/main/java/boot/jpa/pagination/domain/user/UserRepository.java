e45e0f44396a7682fd54d41a949fc4fc1f9e6246
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2019-12-30 11:23:00 +0900

    Modify UserRepository (Page Query)

--- src/main/java/boot/jpa/pagination/domain/user/UserRepository.java
+++ src/main/java/boot/jpa/pagination/domain/user/UserRepository.java
@@ -1,7 +1,16 @@
 package boot.jpa.pagination.domain.user;
 
+import boot.jpa.pagination.dto.UserFindAllDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u " +
+            "from User u " +
+            "order by u.id desc")
+    Page<UserFindAllDto> UserFindAllResponse(Pageable pageable);
 }
 
