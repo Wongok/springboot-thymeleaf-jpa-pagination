1ab76d2415cdb9eb124fbe7763804a4eb01d854f
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2019-12-31 17:33:59 +0900

    Modify UserController (List -> Page)

--- src/main/java/boot/jpa/pagination/controller/UserController.java
+++ src/main/java/boot/jpa/pagination/controller/UserController.java
@@ -1,7 +1,11 @@
 package boot.jpa.pagination.controller;
 
+import boot.jpa.pagination.dto.UserFindAllDto;
 import boot.jpa.pagination.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +22,15 @@ public class UserController {
     }
 
     @GetMapping("/list")
-    public String list(Model model) {
-        model.addAttribute("list", userService.userFindAllResponse());
+    public String list(Model model, @PageableDefault Pageable pageable) {
+        Page<UserFindAllDto> page = userService.userFindAllResponse(pageable);
+        model.addAttribute("page", page);
+        int start = Math.max(1, page.getNumber() - 2);
+        int end = Math.min(start + 4, page.getTotalPages());
+
+        model.addAttribute("start", start);
+        model.addAttribute("end", end);
+
         return "list";
     }
 }
